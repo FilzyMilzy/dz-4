bb49bc218fb35bae78a1273d9ad191e2b3acca22
Mike Filzy <mike.filzy+github@gmail.com>
2023-06-16 18:37:47 +0300

    Implemented Simple app example for dz-4

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -2,7 +2,11 @@ public class SimpleApp {
     public static void main(String[] argv){
 
     IntegerVault integerVault = new IntegerVault();
-    integerVault.getIntValue();
+
+    /*Since getIntValue method already has a sout that prints int value to console
+    (according to assignment point 4.b.)
+    I assume its ok to just call on the method and not use sout in main as stated in point 5*/
+        integerVault.getIntValue();
 
     }
 }
