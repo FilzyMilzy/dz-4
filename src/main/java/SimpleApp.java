dfb3900518c80a0c51b08f8b0559d92f4116fe6f
Mike Filzy <mike.filzy+github@gmail.com>
2023-06-16 18:42:06 +0300

    meaningless commit for branch linkage

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -6,6 +6,7 @@ public class SimpleApp {
     /*Since getIntValue method already has a sout that prints int value to console
     (according to assignment point 4.b.)
     I assume its ok to just call on the method and not use sout in main as stated in point 5*/
+        
         integerVault.getIntValue();
 
     }
